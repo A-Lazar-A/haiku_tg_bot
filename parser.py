9aeb474f1af7fc7e3bcba5a82ae325ffca3efc62
A-Lazar-A <fub.lazar@mail.ru>
2022-07-04 02:15:06 +0300

    change filtering

--- parser.py
+++ parser.py
@@ -8,13 +8,13 @@ async def photo_parser(message):
     ua = UserAgent()
     req = requests.get("https://yandex.ru/images/search?text=" + message, headers={'User-Agent': ua.chrome})
     print(req.text)
-    ph_links = list(filter(lambda x: '.jpg' in x, re.findall('''(?<=["'])[^"']+''', req.text)))
+    ph_links = list(
+        filter(lambda x: x.startswith('http') and x.endswith('.jpg'), re.findall('''(?<=["'])[^"']+''', req.text)))
     print(ph_links)
     ph_list = []
 
     for i in range(len(ph_links)):
-        if ph_links[i][0:4] == "http":
-            ph_list.append(ph_links[i])
+        ph_list.append(ph_links[i])
 
     del ph_links
     im = requests.get(ph_list[randint(0, len(ph_list))])
