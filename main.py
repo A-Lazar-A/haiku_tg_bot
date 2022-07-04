6ea5fb4b057cfec8bfa61196aaf1d1ceb5229faa
A-Lazar-A <fub.lazar@mail.ru>
2022-07-04 03:06:12 +0300

    more random pictures

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from text_on_photo import write_on_photo
 from telebot.async_telebot import AsyncTeleBot
 from telebot import types
 from os import environ, remove
+from random import randint
 
 bot = AsyncTeleBot(environ['BOT_TOKEN'])
 
@@ -21,8 +22,9 @@ async def send_welcome(message):
 @bot.message_handler(func=lambda message: message.text == 'Хокку')
 async def echo_message(message):
     try:
-        await photo_parser(f'Японская живопись')
-        text = ''.join(await rnd_haiku())
+        haiku = await rnd_haiku()
+        await photo_parser(f'Японская живопись {haiku[randint(0,2)]}')
+        text = ''.join(haiku)
         await write_on_photo(text)
         photo = open('temp/img.jpg', 'rb')
         await bot.send_photo(message.from_user.id, photo, caption=text)
